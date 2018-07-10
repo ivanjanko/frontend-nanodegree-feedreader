4c878f79fe1a027171a2aef1d2c1965030a59927
ivan Jankovic <jankoemail@gmail.com>
2018-07-09 20:57:50 -0400

    task 7 done

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
  */
 
 // The names and URLs to all of the feeds we'd like available.
-var allFeedsEdit = [
+var allFeeds = [
     {
         name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
